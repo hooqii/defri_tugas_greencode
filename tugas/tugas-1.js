137dc4a1bd4f0123fcb3962c9f963b608609c543
Miyaoww <email@miyaoww.com>
2023-10-06 00:23:37 +0700

    Menyiapkan Tugas

---
+++ tugas/tugas-1.js
@@ -0,0 +1,29 @@
+// Buatlah tiga variabel dengan nama nama, usia, dan tinggi dan isi dengan informasi pribadi Anda.
+// Buatlah tiga variabel dengan nama bilangan1, bilangan2, dan bilangan3 dan isi dengan angka bebas.
+// Gunakan variabel-variabel tersebut untuk menghitung hasil dari operasi berikut:
+// Penjumlahan dari bilangan1 dan bilangan2.
+// Pengurangan bilangan2 dari bilangan3.
+// Perkalian usia dengan tinggi.
+// Buat variabel isStudent dan isi dengan nilai boolean (true jika Anda seorang mahasiswa, false jika tidak).
+// Buatlah sebuah array dengan nama hobi dan isi dengan beberapa hobi atau kegiatan yang Anda nikmati.
+// Gunakan operasi array untuk menambahkan satu hobi baru ke dalam array hobi.
+// Cetak isi array hobi ke dalam konsol.
+
+let nama = "Defri Salwan"
+let usia = 18
+let tinggi = 150
+let isStudent = true
+
+let bilangan1 = 15
+let bilangan2 = 35
+let bilangan3 = 25
+
+let Penjumlahan = bilangan1+bilangan2
+let Pengurangan = bilangan2-bilangan3
+let Perkalian = usia*tinggi
+
+let hobi = ["Batminton", "Anime", "Game"]
+let hobiBaru = "Renang"
+let menambahkanHobi = hobi.push(hobiBaru)
+
+console.log(hobi)
